455dd4b3115285fc2172c2695544ce4b8fd3cfe1
Vlada Antonchenko <vladajka16@gmail.com>
2020-08-14 21:27:13 -0400

    first commit

---
+++ reactpractice/src/card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+const Project = () => {
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="text-center"> My Projects</h1>
+      </div>
+      <div className="container-fluid mb-5">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <div className="row gy-4">
+              <div className="col-md-4 col-10 mx-auto">
+                <div class="card">
+                  <img src={web} class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" class="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Project;
